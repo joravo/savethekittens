923b976dac3dc865eef74928f158df01e2f49811
Geoff Rees <geoffrees@gm.local>
2017-12-10 15:33:11 -0300

    Made a change
    some pointletss change

--- SaveTheKittensAndAcceptTheHairlessOnes/ViewController.swift
+++ SaveTheKittensAndAcceptTheHairlessOnes/ViewController.swift
@@ -9,10 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var hilly = 500
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
